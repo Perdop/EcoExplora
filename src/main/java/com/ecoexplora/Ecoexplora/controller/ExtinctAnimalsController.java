19f68e54e367e8c806c3bde43e09844b12ebd826
Pedro <operdop@proton.me>
2025-03-11 16:08:06 -0300

    Update ExtinctAnimalsController.java

--- src/main/java/com/ecoexplora/Ecoexplora/controller/ExtinctAnimalsController.java
+++ src/main/java/com/ecoexplora/Ecoexplora/controller/ExtinctAnimalsController.java
@@ -3,6 +3,7 @@ package com.ecoexplora.Ecoexplora.controller;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
