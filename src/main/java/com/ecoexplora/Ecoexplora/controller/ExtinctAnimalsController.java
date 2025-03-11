b6cc95f9f5776b71e36e31c9e5eb5317f07a04f4
Pedro <operdop@proton.me>
2025-03-11 03:27:29 -0300

    Update ExtinctAnimalsController.java

--- src/main/java/com/ecoexplora/Ecoexplora/controller/ExtinctAnimalsController.java
+++ src/main/java/com/ecoexplora/Ecoexplora/controller/ExtinctAnimalsController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*") // Permite requisições de qualquer domínio
 @RestController
-@RestController
 
 public class ExtinctAnimalsController {
 	@Autowired
